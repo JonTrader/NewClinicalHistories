5c811a6d33622817412d4d111cfd708f7e1739b0
JonathanMontoya <montoya0396@gmail.com>
2026-01-16 15:00:12 -0500

    added pages and components folder with its different files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import PatientDetails from './pages/PatientDetails.jsx'
+import Patients from './pages/Patients.jsx'
+import Odontogram from './pages/Odontogram.jsx'
+import Evolution from './pages/Evolution.jsx'
+// import { useAuthStore } from './store/AuthStore.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden'>
+      <Routes>
+        <Route path="/" element={<Patients />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<PatientDetails />} />
+        <Route path="/odontogram/:id" element={<Odontogram />} />
+        <Route path="/evolution/:id" element={<Evolution />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
+    </div>
   )
 }
 
