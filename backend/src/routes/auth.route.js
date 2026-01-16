5fc58ff17e8d3643b4d1790826e4edf32356f22d
Jonathan Montoya <montoya0396@gmail.com>
2026-01-16 09:18:31 -0500

    added rate-limiting and bot protection with arcjet

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,19 @@
 import express from 'express'
-
 import { protectRoute } from '../middleware/auth.middleware.js'
 import { register, login, logout } from '../controllers/auth.controller.js'
+import { arcjetProtection } from '../middleware/arcjet.middleware.js'
 
 const router = express.Router()
+
+// router.get('/test', arcjetProtection, (req, res) => {
+//     res.send('Hello')
+// })
+
+router.use(arcjetProtection)
 
 router.post('/register', register)
 router.post('/login', login)
 router.post('/logout', logout)
-router.get('/hello', (req, res) => {
-    res.send('Hello')
-})
 
 router.get('/check', protectRoute, (req, res) => res.status(200).json(req.user))
 
