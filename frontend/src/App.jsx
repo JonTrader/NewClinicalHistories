17106cdd52f9ae730ec8c101541aa5a2479ade7b
Jonathan Montoya <montoya0396@gmail.com>
2026-01-19 11:42:46 -0500

    changed styling on app.jsx login and register and added new patient page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   if (isCheckingAuth) return <PageLoader />
   return (
-    <div className='min-h-screen bg-[#283E63] relative flex items-center justify-center p-4 overflow-hidden'>
+    <div className='min-h-screen bg-[#283E63] relative'>
       <Routes>
         <Route path="/" element={authUser ? <Patients /> : <Navigate to='/login' />} />
         <Route path="/login" element={!authUser ? <Login /> : <Navigate to='/' />} />
---
+++ frontend/src/pages/NewPatient.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NewPatient() {
+  return (
+    <div>NewPatient</div>
+  )
+}
+
+export default NewPatient
