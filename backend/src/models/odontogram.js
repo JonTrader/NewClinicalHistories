05720b79e327c688cb0987840e5142de41207295
JonathanMontoya <montoya0396@gmail.com>
2026-01-22 23:20:34 -0500

    added small fix to prevent creating objectIds for each tooth

--- backend/src/models/odontogram.js
+++ backend/src/models/odontogram.js
@@ -1,18 +1,20 @@
 import mongoose from 'mongoose'
 
+const toothSchema = new mongoose.Schema({
+    top: String,
+    right: String,
+    bottom: String,
+    left: String,
+    center: String,
+    description: String,
+    number: Number
+}, { _id: false })
+
 
 const OdontogramSchema = new mongoose.Schema({
     tejidosBlandos: [Boolean],
     problemas: [String],
-    teeth: [{
-        top: String,
-        right: String,
-        bottom: String,
-        left: String,
-        center: String,
-        description: String,
-        number: Number
-    }, {_id: false}]
+    teeth: [toothSchema]
 })
 
 const Odontogram = mongoose.model('Odontogram', OdontogramSchema)
