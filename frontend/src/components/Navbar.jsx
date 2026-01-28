267fba87482e75c5dc38f447fa0fef6a00c3ea96
JonathanMontoya <montoya0396@gmail.com>
2026-01-27 22:11:06 -0500

    added search feature on home page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,6 @@ function Navbar() {
                 <Link to="/" className="font-serif text-lightBone text-sm hover:cursor-pointer hover:text-lightOcre hover:scale-110 transition-all">Pacientes</Link>
             </div>
             <div className="navbar-end">
-                <Link to="/new" className="btn mr-4 bg-blueSteel hover:text-lightOcre"><Plus /></Link>
                 <a onClick={logout} className="font-serif text-sm hover:cursor-pointer hover:scale-110 hover:text-lightOcre transition-all">Logout</a>
             </div>
         </div>
