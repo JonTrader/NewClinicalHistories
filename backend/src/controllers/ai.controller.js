e84ee44743abc1a50f1fec1edd1a762520162f17
Jonathan Montoya <montoya0396@gmail.com>
2026-03-13 14:34:17 -0500

    stylized ai summary text

--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -22,7 +22,6 @@ export const generateSummary = async (req, res) => {
                 temperature: 0.0
             }
         })
-        console.log(response.candidates[0].content.parts[0].text)
         return res.status(200).json(response.candidates[0].content.parts[0].text)
     } catch (error) {
         console.error('Error while generating summary by Gemini: ', error)
@@ -30,25 +29,25 @@ export const generateSummary = async (req, res) => {
     }
 }
 
-export const generateEvolution = async (req, res) => {
-    try {
-        if (!req.body.text) {
-            return res.status(400).json({ message: 'No text provided' })
-        }
-        const response = await ai.models.generateContent({
-            model: 'gemini-2.5-flash-lite',
-            contents: `
-            Escribe una evolucion para este paciente segun las palabras claves delimitadas por guiones bajos.
-            _${req.body.text}_
-            `,
-            config: {
-                temperature: 0.0
-            }
-        })
-        console.log(response.candidates[0].content.parts[0].text)
-        return res.status(200).json(response.candidates[0].content.parts[0].text)
-    } catch (error) {
-        console.error('Error while generating summary by Gemini: ', error)
-        res.status(500).json({ message: 'Internal server error trying to generate summary.' })
-    }
-}
+// export const generateEvolution = async (req, res) => {
+//     try {
+//         if (!req.body.text) {
+//             return res.status(400).json({ message: 'No text provided' })
+//         }
+//         const response = await ai.models.generateContent({
+//             model: 'gemini-2.5-flash-lite',
+//             contents: `
+//             Escribe una evolucion para este paciente segun las palabras claves delimitadas por guiones bajos.
+//             _${req.body.text}_
+//             `,
+//             config: {
+//                 temperature: 0.0
+//             }
+//         })
+//         console.log(response.candidates[0].content.parts[0].text)
+//         return res.status(200).json(response.candidates[0].content.parts[0].text)
+//     } catch (error) {
+//         console.error('Error while generating summary by Gemini: ', error)
+//         res.status(500).json({ message: 'Internal server error trying to generate summary.' })
+//     }
+// }
